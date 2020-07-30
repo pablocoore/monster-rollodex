d265bc1f3537e8bc5d1fbb9bffb2a3f2d630f16b
pablocoore <8518836+pablocoore@users.noreply.github.com>
2020-07-29 21:44:16 -0300

    Updates

---
+++ precache-manifest.a9e2c2157d038dc40236c7529a47698f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b59ddaf6a05ee2301c0c1fbfc7f9f4",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "860e0a713a5899c2441e",
+    "url": "/monster-rollodex/static/css/main.cba21718.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rollodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rollodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860e0a713a5899c2441e",
+    "url": "/monster-rollodex/static/js/main.068aaf30.chunk.js"
+  },
+  {
+    "revision": "ff6ec000871ecec21535",
+    "url": "/monster-rollodex/static/js/runtime-main.f76b0d0f.js"
+  }
+]);
